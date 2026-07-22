aade83e03f9b760011600ae914b50d5a960237aa
CT-12 <ben23515225566@gmail.com>
2026-07-23 00:27:28 +0800

    Add a CORS

---
+++ src/main/java/com/ctailee/backend/CorsConfig.java
@@ -0,0 +1,33 @@
+package com.ctailee.backend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                // 哪些後端 API 路徑要套用這份 CORS 設定。/** 代表所有路徑。
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "https://ctailee.com",
+                                "https://api.ctailee.com"
+                        )
+                        .allowedMethods(
+                                "GET",
+                                "POST",
+                                "PUT",
+                                "DELETE",
+                                "OPTIONS"
+                        )
+                        .allowedHeaders("*"); // 前端請求可以帶哪些 request headers。* 代表全部
+            }
+        };
+    }
+}
